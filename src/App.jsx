b71f06677c5d54717dbc5b4183a912da303365a0
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-09 16:30:07 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  return <h1>Proof of Life</h1>;
+}
+
+export default App;

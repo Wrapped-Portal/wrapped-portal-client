1f05dcb0e5870e4b038cd6f98de12df48a38e248
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-09 20:21:16 -0500

    setup test route

--- src/store/middleware/getToken.js
+++ src/store/middleware/getToken.js
@@ -1,13 +1,15 @@
 /** @format */
 
+import axios from 'axios';
+
 const getToken = (store) => (next) => async (action) => {
   if (action.type === 'login/storeToken') {
     try {
-      console.log(action.payload.code);
+      console.log('TEST', import.meta.env.VITE_SERVER_URI);
       const results = await axios.get(import.meta.env.VITE_SERVER_URI, {
         code: action.payload.code,
       });
-      console.log(results);
+      console.log(results.data);
     } catch (e) {
       console.error('Error: GetToken Middleware:', e.message);
       next(action);

fad61b77c08e1d7f6b46b37b2661fe9111d46a18
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-15 17:59:58 -0500

    add config file for scope of permissions

---
+++ config.js
@@ -0,0 +1,17 @@
+/** @format */
+
+export const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+export const RESPONSE_TYPE = 'code';
+export const SCOPE = [
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'streaming',
+  'user-read-email',
+  'user-read-private',
+  'user-library-read',
+  'user-library-modify',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-top-read',
+  'user-read-currently-playing',
+].join('%20');

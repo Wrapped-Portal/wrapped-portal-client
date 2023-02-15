3ecd1f091383d61d56f47a61482be8a67d053fa1
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-14 20:50:06 -0500

    create soundBoard

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -3,13 +3,14 @@
 import React from 'react';
 
 import CustomRec from './CustomRec';
+import SoundBoard from './SoundBoard';
 import UserTopResults from './UserTopResults';
 
 export default function Dashboard() {
   return (
     <>
       <main>
-        
+        <SoundBoard />
         <div className="dashboard">
           <div className="column">
             <UserTopResults></UserTopResults>

bbcd926e7b5eae4853908d138f63344637b1e0de
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-14 19:28:38 -0500

    fix linter issues - add double quotes

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,26 +1,24 @@
 /** @format */
 
 import React from 'react';
+
 import CustomRec from './CustomRec';
 import UserTopResults from './UserTopResults';
 
-
 export default function Dashboard() {
-
-
   return (
     <>
       <main>
-    <div className='dashboard'>
-      <div className='column'>
-      <UserTopResults></UserTopResults>
-      </div>
-      <div className='column'>
-      <CustomRec></CustomRec>
-      </div>
-      <div className='column'>
+        
+        <div className="dashboard">
+          <div className="column">
+            <UserTopResults></UserTopResults>
+          </div>
+          <div className="column">
+            <CustomRec></CustomRec>
+          </div>
+          <div className="column"></div>
         </div>
-      </div>
       </main>
     </>
   );

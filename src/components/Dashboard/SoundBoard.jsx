3012107ae138f0f74b8be2920b349560d00b1694
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-15 20:15:19 -0500

    add labels to soundboard

--- src/components/Dashboard/SoundBoard.jsx
+++ src/components/Dashboard/SoundBoard.jsx
@@ -1,40 +1,43 @@
 /** @format */
 
 import React from 'react';
-import { useSelector } from 'react-redux';
 import Knob from '../Knob';
 
 export default function SoundBoard() {
-  const { dance, energy, loud, vibe, tempo, popular, instrumental } =
-    useSelector((state) => state.soundBoardSlice);
-  console.log(dance, energy, loud, vibe, tempo, popular, instrumental);
   return (
     <div className="sound-board">
       <Knob
+        label="Dancability"
         bgColor={50}
         fieldName="dance"
       />
       <Knob
+        label="Energy"
         bgColor={150}
         fieldName="energy"
       />
       <Knob
+        label="Loudness"
         bgColor={250}
         fieldName="loud"
       />
       <Knob
+        label="Vibe"
         bgColor={350}
         fieldName="vibe"
       />
       <Knob
+        label="Tempo"
         bgColor={100}
         fieldName="tempo"
       />
       <Knob
+        label="Popularity"
         bgColor={200}
         fieldName="popular"
       />
       <Knob
+        label="Instrumental"
         bgColor={300}
         fieldName="instrumental"
       />

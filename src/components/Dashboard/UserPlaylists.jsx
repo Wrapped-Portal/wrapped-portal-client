41a380dbf8dbad762c84ff1bc717085461ea2b2b
Stevenrej <steveo732@gmail.com>
2023-02-23 15:47:50 -0600

    remove functionality added to playlists

--- src/components/Dashboard/UserPlaylists.jsx
+++ src/components/Dashboard/UserPlaylists.jsx
@@ -1,16 +1,38 @@
 /** @format */
 
-import { useSelector} from 'react-redux';
+import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
 import React from 'react';
 import {
+  Button,
   Paper,
   Text,
   List,
 } from '@mantine/core';
+import { selectTrack } from '../../store/reducers/playlistSlice';
+
 export default function UserPlaylists() {
-  const { playlistItems } = useSelector(state => state.playlistSlice,
+  const { token } = useSelector((state) => state.login);
+  const { playlistItems, selectedPlaylist, selectedTrack  } = useSelector(state => state.playlistSlice,
   );
 
+  const dispatch = useDispatch();
+  
+  const handleRemoveTrackFromPlaylist = async (trackUri) => {
+    dispatch(selectTrack(trackUri));
+    try {
+      const response = await axios.delete(`http://localhost:3001/remove?token=${token.accessToken}`, {
+        data: {
+          playlistId: selectedPlaylist,
+          trackUri: trackUri,
+        }
+      });
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
 
   return (
     <>
@@ -24,14 +46,24 @@ export default function UserPlaylists() {
         >
           <List type="ordered" className='list' >
             {playlistItems?.items.map((item, index) => (
-                <List.Item key={`item-${index}`} className='list_item'>
-                  <Text fw={700}>
+              <List.Item key={`item-${index}`} className='list_item'>
+                <Text fw={700}>
                   {item?.track.name}
-                  </Text>
-                  <Text fz="sm" c="dimmed">
+                </Text>
+                <Text fz="sm" c="dimmed">
                   {item?.track?.artists[0].name}
-                  </Text>
-                </List.Item>
+                </Text>
+                <Button
+                  key={`button-${index}`}
+                  color="red"
+                  radius="sm"
+                  size="xs"
+                  compact
+                  onClick={() => handleRemoveTrackFromPlaylist(item?.track.uri)}
+                >
+                  X
+                </Button>
+              </List.Item>
             ))}
           </List>
         </Paper>

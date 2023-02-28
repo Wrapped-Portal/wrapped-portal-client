c9b948b65b0cdf5ba08a0fcbdf8ba2ffac78f556
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-02-28 17:09:55 -0500

    add popover menu on knobs

--- src/components/Dashboard/SoundBoard.jsx
+++ src/components/Dashboard/SoundBoard.jsx
@@ -7,36 +7,43 @@ export default function SoundBoard() {
   return (
     <div className="sound-board">
       <Knob
+        description="Generic text that describes what is happening"
         label="Dancability"
         bgColor={50}
         fieldName="dance"
       />
       <Knob
+        description="Generic text that describes what is happening"
         label="Energy"
         bgColor={150}
         fieldName="energy"
       />
       <Knob
+        description="Generic text that describes what is happening"
         label="Loudness"
         bgColor={250}
         fieldName="loud"
       />
       <Knob
+        description="Generic text that describes what is happening"
         label="Vibe"
         bgColor={350}
         fieldName="vibe"
       />
       <Knob
+        description="Generic text that describes what is happening"
         label="Tempo"
         bgColor={100}
         fieldName="tempo"
       />
       <Knob
+        description="Generic text that describes what is happening"
         label="Popularity"
         bgColor={200}
         fieldName="popular"
       />
       <Knob
+        description="Generic text that describes what is happening"
         label="Instrumental"
         bgColor={300}
         fieldName="instrumental"
--- src/components/Knob/index.jsx
+++ src/components/Knob/index.jsx
@@ -1,11 +1,14 @@
 /** @format */
 
+import { Popover, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import React, { useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { setFieldValue } from '../../store/reducers/soundBoardSlice';
 import { getDeg, convertRange, renderTicks } from './helpers';
 
-export default function Knob({ fieldName, bgColor, label }) {
+export default function Knob({ fieldName, bgColor, label, description }) {
+  const [opened, { close, open }] = useDisclosure(false);
   const [deg, setDeg] = useState(0);
   const fullAngle = 260;
   const startAngle = (360 - fullAngle) / 2;
@@ -72,72 +75,95 @@ export default function Knob({ fieldName, bgColor, label }) {
   };
 
   return (
-    <div className="knob__wrapper">
-      <label
-        className="knob__label"
-        htmlFor={label}
-      >
-        {label}
-      </label>
-
-      <div
-        id={label}
-        className="knob"
-        style={{ width: size, height: size }}
-        role="slider"
-        tabIndex="0"
-        aria-valuemin="1"
-        aria-valuemax="100"
-        aria-valuenow={Math.floor(
-          convertRange(startAngle, endAngle, 1, 100, currentDeg),
-        )}
-        onKeyDown={handleKeyDown}
-      >
-        <div className="ticks">
-          {numTicks
-            ? renderTicks(
-                fullAngle,
-                numTicks,
-                size,
-                margin,
-                startAngle,
-                endAngle,
-              ).map((tick, i) => (
-                <div
-                  key={i}
-                  className={'tick' + (tick.deg <= currentDeg ? ' active' : '')}
-                  style={tick.tickStyle}
-                />
-              ))
-            : null}
-        </div>
+    <Popover
+      width={200}
+      position="bottom"
+      withArrow
+      shadow="md"
+      opened={opened}
+    >
+      <Popover.Target>
         <div
-          className="knob outer"
-          style={{
-            width: size,
-            height: size,
-            margin: margin,
-            backgroundImage: color
-              ? `radial-gradient(100% 70%,hsl(${bgColor}, ${currentDeg}%, ${
-                  currentDeg / 5
-                }%),hsl(${Math.random() * 100},20%,${currentDeg / 36}%))`
-              : null,
-          }}
-          ref={knobRef}
-          onMouseDown={startDrag}
+          onMouseDown={close}
+          onMouseEnter={open}
+          onMouseLeave={close}
         >
-          <div
-            className="knob inner"
-            style={{
-              width: size,
-              height: size,
-              transform: `rotate(${currentDeg}deg)`,
-            }}
-          >
-            <div className="grip" />
+          <div className="knob__wrapper">
+            <label
+              className="knob__label"
+              htmlFor={label}
+            >
+              {label}
+            </label>
+
+            <div
+              id={label}
+              className="knob"
+              style={{ width: size, height: size }}
+              role="slider"
+              tabIndex="0"
+              aria-valuemin="1"
+              aria-valuemax="100"
+              aria-valuenow={Math.floor(
+                convertRange(startAngle, endAngle, 1, 100, currentDeg),
+              )}
+              onKeyDown={handleKeyDown}
+            >
+              <div className="ticks">
+                {numTicks
+                  ? renderTicks(
+                      fullAngle,
+                      numTicks,
+                      size,
+                      margin,
+                      startAngle,
+                      endAngle,
+                    ).map((tick, i) => (
+                      <div
+                        key={i}
+                        className={
+                          'tick' + (tick.deg <= currentDeg ? ' active' : '')
+                        }
+                        style={tick.tickStyle}
+                      />
+                    ))
+                  : null}
+              </div>
+              <div
+                className="knob outer"
+                style={{
+                  width: size,
+                  height: size,
+                  margin: margin,
+                  backgroundImage: color
+                    ? `radial-gradient(100% 70%,hsl(${bgColor}, ${currentDeg}%, ${
+                        currentDeg / 5
+                      }%),hsl(${Math.random() * 100},20%,${currentDeg / 36}%))`
+                    : null,
+                }}
+                ref={knobRef}
+                onMouseDown={startDrag}
+              >
+                <div
+                  className="knob inner"
+                  style={{
+                    width: size,
+                    height: size,
+                    transform: `rotate(${currentDeg}deg)`,
+                  }}
+                >
+                  <div className="grip" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Popover.Target>
+      {description && (
+        <Popover.Dropdown sx={{ pointerEvents: 'none' }}>
+          <Text size="sm">{description}</Text>
+        </Popover.Dropdown>
+      )}
+    </Popover>
   );
 }

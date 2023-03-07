77a33d4bd2678a32cfc1e295b19676fc1ea537fb
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-07 15:02:36 -0500

    add spotify logo and links for each track

--- src/components/Dashboard/UserTopResults.jsx
+++ src/components/Dashboard/UserTopResults.jsx
@@ -28,9 +28,7 @@ export default function UserTopResults() {
   const { token } = useSelector((state) => state.login);
   const { user } = useSelector((state) => state.userSlice);
   const { selectedData } = useSelector((state) => state.selectedSlice);
-  const { selectedPlaylist } = useSelector(
-    (state) => state.playlistSlice,
-  );
+  const { selectedPlaylist } = useSelector((state) => state.playlistSlice);
 
   const dispatch = useDispatch();
 
@@ -62,40 +60,40 @@ export default function UserTopResults() {
       }
     })();
   }, [type, range]);
-  
-const handleCreateTopPlaylist = async () => {
-  let time = '';
-  if (range === 'short_term') {
-    time = 'last month'
-  }
-  if (range === 'medium_term') {
-    time = 'last 6 months'
-  }
-  if (range === 'long_term') {
-    time = 'All Time'
-  }
 
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
-  const yyyy = today.getFullYear();
-  const date = `${mm}/${dd}/${yyyy}`;
+  const handleCreateTopPlaylist = async () => {
+    let time = '';
+    if (range === 'short_term') {
+      time = 'last month';
+    }
+    if (range === 'medium_term') {
+      time = 'last 6 months';
+    }
+    if (range === 'long_term') {
+      time = 'All Time';
+    }
 
-  const body = {
-    name: `Top Tracks ${date} (${time}) `,
-    description: `Your favorite tracks ${time} as of ${date}. Created on Tune Port`,
-    public: true,
-    user_id: user.id,
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
+    const date = `${mm}/${dd}/${yyyy}`;
+
+    const body = {
+      name: `Top Tracks ${date} (${time}) `,
+      description: `Your favorite tracks ${time} as of ${date}. Created on Tune Port`,
+      public: true,
+      user_id: user.id,
+    };
+    const uris = data?.items.map((item) => item.uri).join(',');
+
+    let payload = {
+      body: body,
+      uris: uris,
+    };
+
+    dispatch(createCustomPlaylist(payload));
   };
-  const uris = data?.items.map((item) => item.uri).join(',');
-
-  let payload = {
-    body: body,
-    uris: uris,
-  }
-
-  dispatch(createCustomPlaylist(payload));
-};
 
   return (
     <>
@@ -105,13 +103,38 @@ const handleCreateTopPlaylist = async () => {
           onClose={() => setOpened(false)}
           title={`${selectedData.tracks[2].artists[0].name}'s Top Tracks`}
         >
-          <List type="ordered" className="list">
-            {selectedData?.tracks.map((item, index) =>
+          <List
+            type="ordered"
+            className="list"
+          >
+            {selectedData?.tracks.map((item, index) => (
               <List.Item
                 key={`item-${index}`}
                 className="list_item"
               >
-                <Group onClick={() => dispatch(playSong(item.uri))}>
+                <Group
+                  px="18px"
+                  onClick={() => dispatch(playSong(item.uri))}
+                >
+                  <a
+                    href={item.external_urls.spotify}
+                    className="logo logo--top-tracks"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 48 48"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M38.1966 21.2764C30.4604 16.6821 17.6998 16.2597 10.3146 18.5011C9.12845 18.861 7.87433 18.1915 7.51523 17.0056C7.15585 15.8189 7.82446 14.5656 9.01123 14.2051C17.4891 11.6318 31.5819 12.1288 40.4879 17.4155C41.5549 18.0488 41.9045 19.4265 41.2723 20.4914C40.6395 21.5581 39.2607 21.9098 38.1966 21.2764ZM37.9433 28.0812C37.4005 28.9619 36.249 29.2381 35.3694 28.6973C28.92 24.733 19.0848 23.5843 11.4544 25.9005C10.4651 26.1994 9.41991 25.6417 9.11927 24.6539C8.82122 23.6645 9.37921 22.6214 10.3671 22.3204C19.0837 19.6752 29.9199 20.9563 37.3277 25.5088C38.2072 26.0504 38.4841 27.2025 37.9433 28.0812ZM35.0066 34.6163C34.5753 35.3236 33.6545 35.5451 32.9498 35.1141C27.314 31.6696 20.2203 30.8918 11.8662 32.7999C11.0612 32.9844 10.2588 32.48 10.0753 31.6753C9.89077 30.8703 10.3935 30.0678 11.2002 29.8841C20.3424 27.7943 28.1844 28.6936 34.5102 32.5591C35.2155 32.9899 35.4376 33.9113 35.0066 34.6163ZM23.9999 0C10.7454 0 0 10.7451 0 23.9996C0 37.2555 10.7454 48 23.9999 48C37.2549 48 48 37.2555 48 23.9996C48 10.7451 37.2549 0 23.9999 0Z"
+                        fill="black"
+                      />
+                    </svg>
+                  </a>
                   <img
                     className="play_button-icon"
                     src="https://cdn-icons-png.flaticon.com/512/0/375.png"
@@ -152,7 +175,7 @@ const handleCreateTopPlaylist = async () => {
                   +
                 </Button>
               </List.Item>
-            )}
+            ))}
           </List>
         </Modal>
       )}
@@ -224,10 +247,33 @@ const handleCreateTopPlaylist = async () => {
                 {data?.items.map((item, index) =>
                   item.album ? (
                     <List.Item
+                      mx="42px"
                       key={`item-${index}`}
                       className="list_item"
                     >
-                      <Group onClick={() => dispatch(playSong(item.uri))}>
+                      <Group
+                      
+                        onClick={() => dispatch(playSong(item.uri))}
+                      >
+                        <a
+                          href={item.external_urls.spotify}
+                          className="logo logo--top-tracks"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 48 48"
+                            fill="none"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              clipRule="evenodd"
+                              d="M38.1966 21.2764C30.4604 16.6821 17.6998 16.2597 10.3146 18.5011C9.12845 18.861 7.87433 18.1915 7.51523 17.0056C7.15585 15.8189 7.82446 14.5656 9.01123 14.2051C17.4891 11.6318 31.5819 12.1288 40.4879 17.4155C41.5549 18.0488 41.9045 19.4265 41.2723 20.4914C40.6395 21.5581 39.2607 21.9098 38.1966 21.2764ZM37.9433 28.0812C37.4005 28.9619 36.249 29.2381 35.3694 28.6973C28.92 24.733 19.0848 23.5843 11.4544 25.9005C10.4651 26.1994 9.41991 25.6417 9.11927 24.6539C8.82122 23.6645 9.37921 22.6214 10.3671 22.3204C19.0837 19.6752 29.9199 20.9563 37.3277 25.5088C38.2072 26.0504 38.4841 27.2025 37.9433 28.0812ZM35.0066 34.6163C34.5753 35.3236 33.6545 35.5451 32.9498 35.1141C27.314 31.6696 20.2203 30.8918 11.8662 32.7999C11.0612 32.9844 10.2588 32.48 10.0753 31.6753C9.89077 30.8703 10.3935 30.0678 11.2002 29.8841C20.3424 27.7943 28.1844 28.6936 34.5102 32.5591C35.2155 32.9899 35.4376 33.9113 35.0066 34.6163ZM23.9999 0C10.7454 0 0 10.7451 0 23.9996C0 37.2555 10.7454 48 23.9999 48C37.2549 48 48 37.2555 48 23.9996C48 10.7451 37.2549 0 23.9999 0Z"
+                              fill="black"
+                            />
+                          </svg>
+                        </a>
                         <img
                           className="play_button-icon"
                           src="https://cdn-icons-png.flaticon.com/512/0/375.png"
@@ -275,7 +321,7 @@ const handleCreateTopPlaylist = async () => {
                           dispatch(getArtistTop(item.id));
                           setOpened(true);
                         }}
-                        className='hover'
+                        className="hover"
                       >
                         <Text
                           fw={600}
@@ -297,14 +343,14 @@ const handleCreateTopPlaylist = async () => {
               </List>
             </Paper>
             {data?.items[0].album && (
-            <Button
-            className='top_playlist_button'
-              variant="gradient"
-              gradient={{ from: 'teal', to: 'lime', deg: 105 }}
-              onClick={() => handleCreateTopPlaylist()}
-            >
-              Create Playlist For Top Tracks
-            </Button>
+              <Button
+                className="top_playlist_button"
+                variant="gradient"
+                gradient={{ from: 'teal', to: 'lime', deg: 105 }}
+                onClick={() => handleCreateTopPlaylist()}
+              >
+                Create Playlist For Top Tracks
+              </Button>
             )}
           </>
         )}

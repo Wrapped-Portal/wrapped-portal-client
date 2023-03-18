ef1528a18bebc37f037c98b2727ebc6a8642fd32
Stevenrej <steveo732@gmail.com>
2023-03-18 14:34:25 -0500

    svg added to features button

--- src/components/Dashboard/UserTopResults.jsx
+++ src/components/Dashboard/UserTopResults.jsx
@@ -343,7 +343,7 @@ export default function UserTopResults() {
                           setOpenFeatures(true);
                         }}
                       >
-                        ?
+                        <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 512 512"><path fill="currentColor" d="M128 496H48V304h80Zm224 0h-80V208h80Zm112 0h-80V96h80Zm-224 0h-80V16h80Z" /></svg>
                       </Button>
                     </List.Item>
                   ) : (

246227fc630fbf971519336b50a99123c87ee4e3
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-18 17:35:55 -0500

    remove extra console.log

--- src/store/middleware/recentlyPlayed.js
+++ src/store/middleware/recentlyPlayed.js
@@ -5,13 +5,12 @@ import axios from 'axios';
 const getRecent = (store) => (next) => async (action) => {
   if (action.type === 'webPlayer/setTrackUri') {
     try {
-      console.log('getting song');
       const results = await axios.post(
         `${import.meta.env.VITE_SERVER_URI}recent`,
         { token: store.getState().login.token.accessToken },
       );
       action.payload = { uri: results.data };
-      console.log(action.payload);
+
       next(action);
     } catch (e) {
       console.error('Error: get recent Middleware:', e.message, action.payload);

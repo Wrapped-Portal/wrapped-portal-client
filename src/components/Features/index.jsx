8530c02440c85152752ccd54cda2df1a5f735f1a
Stevenrej <steveo732@gmail.com>
2023-03-18 15:38:22 -0500

    updated graph to have popularity

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -4,17 +4,16 @@ import { CChart } from '@coreui/react-chartjs';
 import { Group } from '@mantine/core';
 
 export default function Features() {
-  const { audioFeatures } = useSelector((state) => state.audioFeaturesSlice);
+  const { audioFeatures, audioArtist } = useSelector((state) => state.audioFeaturesSlice);
 
   function msToMinSec(duration_ms) {
     const minutes = Math.floor(duration_ms / 60000);
     const seconds = ((duration_ms % 60000) / 1000).toFixed(0);
     return `${minutes}:${seconds.padStart(2, '0')}`;
   }
-  
+
   const duration_ms = audioFeatures?.duration_ms;
   const duration_formatted = msToMinSec(duration_ms);
-  
 
   const musicalKeys = [
     "No key",
@@ -31,7 +30,7 @@ export default function Features() {
     "A#/Bb",
     "B",
   ];
-  
+
   const keyIndex = audioFeatures?.key;
   let keyName;
   if (keyIndex === -1) {
@@ -65,29 +64,41 @@ export default function Features() {
           },
         }}
         data={{
-          labels: ['Acousticness', 'Danceability', 'Energy', 'Instrumentalness', 'Liveness', 'Speechiness', 'Vibe', 'Loudness'],
+          labels: [
+            'Acousticness',
+            'Danceability',
+            'Energy',
+            'Liveness',
+            'Speechiness',
+            'Vibe',
+            'Loudness',
+            'Popularity',
+            'Instrumentalness',
+          ],
           datasets: [
             {
               label: 'Feature Rating',
               backgroundColor: [
                 'rgba(0, 128, 255, 0.7)',   // Blue
-                'rgba(255, 51, 51, 0.7)',   // Red
                 'rgba(0, 204, 102, 0.7)',   // Green
-                'rgba(255, 204, 0, 0.7)',   // Yellow
+                'rgba(255, 51, 51, 0.7)',   // Red
                 'rgba(51, 204, 204, 0.7)',  // Cyan
+                'rgba(255, 204, 0, 0.7)',   // Yellow
                 'rgba(254, 0, 204, 0.7)',   // Magenta
                 'rgba(255, 128, 0, 0.7)',   // Orange
+                'rgba(0, 176, 150, 0.7)',    // Teal
                 'rgba(153, 51, 255, 0.7)',  // Purple
               ],
               data: [
                 audioFeatures?.acousticness * 100,
                 audioFeatures?.danceability * 100,
                 audioFeatures?.energy * 100,
-                audioFeatures?.instrumentalness * 100,
                 audioFeatures?.liveness * 100,
                 audioFeatures?.speechiness * 100,
                 audioFeatures?.valence * 100,
                 loudnessScaled.toFixed(2),
+                audioArtist?.popularity,
+                audioFeatures?.instrumentalness * 100,
               ],
             },
           ],
